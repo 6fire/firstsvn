c7914561420aa919b2719bf543e0979424adadd3
Administrator <sfd@sa.com>
2018-02-07 17:50:23 +0800

    第一次提交

---
+++ src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test {
+    int x=99;
+   public void print(){}
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public static void main(String[] args) {
+      new Test();
+    }
+}
